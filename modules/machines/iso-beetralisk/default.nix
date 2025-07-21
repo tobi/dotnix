23de1b80905efd9eb1a0cda1b05c18fea61190c7
Tobi Lütke <tobi@lutke.com>
2025-07-21 17:11:27 -0400

    cleanup

--- modules/machines/iso-beetralisk/default.nix
+++
@@ -1,6 +0,0 @@
-{ inputs, ... }:
-{
-  imports = [
-    ./configuration.nix
-  ];
-}
--- utils/utils.nix
+++ utils/utils.nix
@@ -3,12 +3,14 @@
 {
   mkMachines = { inputs }:
     let
-      machinesPath = ./modules/machines;
+      machinesPath = ../modules/machines;
       machineNames = builtins.attrNames (builtins.readDir machinesPath);
-      machineDirs = builtins.filter (name: 
-        (builtins.readDir machinesPath).${name} == "directory"
-      ) machineNames;
-      
+      machineDirs = builtins.filter
+        (name:
+          (builtins.readDir machinesPath).${name} == "directory"
+        )
+        machineNames;
+
       pkgs = import nixpkgs {
         system = "x86_64-linux";
         config.allowUnfree = true;
@@ -17,18 +19,20 @@
         ];
       };
     in
-    builtins.listToAttrs (map (name: {
-      name = name;
-      value = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        inherit pkgs;
-        specialArgs = {
-          inherit inputs;
-          home-manager = inputs.home-manager;
+    builtins.listToAttrs (map
+      (name: {
+        name = name;
+        value = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          inherit pkgs;
+          specialArgs = {
+            inherit inputs;
+            home-manager = inputs.home-manager;
+          };
+          modules = [
+            (machinesPath + "/${name}/default.nix")
+          ];
         };
-        modules = [
-          (machinesPath + "/${name}/default.nix")
-        ];
-      };
-    }) machineDirs);
+      })
+      machineDirs);
 }
